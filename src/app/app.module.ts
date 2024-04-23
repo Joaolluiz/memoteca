1108838cf1eb09abfd575861a2a21224b4b818a4
Joaolluiz <joao.fdk8@gmail.com>
2024-04-23 14:52:41 +0000

    Implementado two way binding no formulário para fluxo de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { CriarPensamentoComponent } from './components/pensamentos/criar-pensame
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
